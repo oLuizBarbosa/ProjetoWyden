e13366e0e7f017179a6443b2be6b5b22300da818
Guilherme Miguel N. S. R. <miguelsantarosa355@gmail.com>
2025-06-02 17:31:57 -0300

    Delete src directory

--- src/main/java/com/wyden/ProjetoWyden/repository/CadastroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wyden.ProjetoWyden.repository;
-
-import com.wyden.ProjetoWyden.models.Cadastro;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CadastroRepository extends CrudRepository<Cadastro, Long> {
-}
